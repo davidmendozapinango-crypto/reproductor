1819be2fb65e1fbc993fe63cb6bdd8f0edbe597b
davidmendozapinango-crypto <david.mendoza.pinango@gmail.com>
2026-05-10 15:26:25 -0400

    Funciones de Imprimir Menu y Leer archivo

--- include/cancion.h
+++ include/cancion.h
@@ -1,5 +1,6 @@
 #ifndef CANCION_H
 #define CANCION_H
+#include <string.h>
 
 
 typedef struct Cancion { //Defino una canción como un nodo de lista doblemente enlazada.
@@ -15,4 +16,18 @@ typedef struct ListaDoble {
     int tamano;   // Cantidad de elementos
 } ListaDoble;
 
+void Menu(){//Imprimir menu
+    printf("------MENU------\n - play (nombre de lista o cancion)\n - queue (nombre de lista o cancion)\n - new (nombre de lista): (nombre cancion) - (nombre cancion) \n - next \n - back \n - shuffle\n - loop\n - clear queue\n - clear history\n - catalog\n - lists\n - queue\n (q para salir)\n");
+
+}
+
+void leerArchivo(char *nombre){ //Para leer el archivo e identificar catalogo y listas.
+    FILE *ptrA = fopen(nombre, "r");
+    
+    if (ptrA == NULL){
+        printf("Error: No se pudo abrir el archivo '%s'\n", nombre);
+        return;
+    }
+
+}
 #endif /*CANCION_H*/
