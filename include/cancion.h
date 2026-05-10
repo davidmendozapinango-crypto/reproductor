a6e3b049115647a92a3e6f0f32cfb5539cc22ef6
Albert <albertandres08@email.com>
2026-05-10 17:32:10 -0400

    mover archivos de funciones a cancion.c

--- include/cancion.h
+++ include/cancion.h
@@ -15,38 +15,4 @@ typedef struct ListaDoble {
     int tamano;   
 } ListaDoble;
 
-void leerArchivo(char *nombre){ //Para leer el archivo e identificar catalogo y listas.
-    FILE *ptrA = fopen(nombre, "r");
-    if (ptrA == NULL) { //Verificación de que se pudo abrir el archivo
-        perror("Error al abrir");
-        return;
-    }
-    char linea[600];
-    while (fgets(linea, sizeof(linea), ptrA)){
-        if (strstr(linea, "Catálogo:") != NULL){
-            char *token = strtok(linea, ":");
-            token = strtok(NULL, " - \n\r");
-            while (token != NULL){
-                //LLAMADA A LA FUNCION DE NUEVA CANCION(MALLOC)
-                printf("Cancion encontrada: %s\n", token);
-                token = strtok(NULL, " - \n\r");
-            }
-        }
-    }
-}
-
-Cancion* crearCancion(char* nombrePista, float duracionPista) {//crea nodo cancion
-    Cancion* new = (Cancion*)malloc(sizeof(Cancion));
-    if (new == NULL) {
-        printf("Error: No hay memoria suficiente para crear la canción.\n");
-        return NULL;
-    }
-    strncpy(nuevo->nombre, nombrePista, 99);
-    nuevo->nombre[99] = '\0'; 
-    nuevo->duracion = duracionPista;
-    nuevo->sig = NULL;
-    nuevo->ant = NULL;
-    return new;
-}
-
 #endif /*CANCION_H*/
--- src/cancion.c
+++ src/cancion.c
@@ -10,3 +10,37 @@ void insertarCancion(Cancion **cabeza, char *nombre) {
     nueva->sig = *cabeza;
     *cabeza = nueva;
 }
+
+void leerArchivo(char *nombre){ //Para leer el archivo e identificar catalogo y listas.
+    FILE *ptrA = fopen(nombre, "r");
+    if (ptrA == NULL) { //Verificación de que se pudo abrir el archivo
+        perror("Error al abrir");
+        return;
+    }
+    char linea[600];
+    while (fgets(linea, sizeof(linea), ptrA)){
+        if (strstr(linea, "Catálogo:") != NULL){
+            char *token = strtok(linea, ":");
+            token = strtok(NULL, " - \n\r");
+            while (token != NULL){
+                //LLAMADA A LA FUNCION DE NUEVA CANCION(MALLOC)
+                printf("Cancion encontrada: %s\n", token);
+                token = strtok(NULL, " - \n\r");
+            }
+        }
+    }
+}
+
+Cancion* crearCancion(char* nombrePista, float duracionPista) {//crea nodo cancion
+    Cancion* new = (Cancion*)malloc(sizeof(Cancion));
+    if (new == NULL) {
+        printf("Error: No hay memoria suficiente para crear la canción.\n");
+        return NULL;
+    }
+    strncpy(nuevo->nombre, nombrePista, 99);
+    nuevo->nombre[99] = '\0'; 
+    nuevo->duracion = duracionPista;
+    nuevo->sig = NULL;
+    nuevo->ant = NULL;
+    return new;
+}
