257a51f8b63a5af2474daa5d3349a93ed1f62d64
davidmendozapinango-crypto <david.mendoza.pinango@gmail.com>
2026-05-10 15:32:12 -0400

    Verificación de lectura del archivo

--- include/cancion.h
+++ include/cancion.h
@@ -11,9 +11,9 @@ typedef struct Cancion { //Defino una canción como un nodo de lista doblemente 
 } Cancion;
 
 typedef struct ListaDoble {
-    Cancion* cabeza; // Puntero al primer elemento
-    Cancion* cola;   // Puntero al último elemento (facilita insertar al final)
-    int tamano;   // Cantidad de elementos
+    Cancion* cabeza;
+    Cancion* cola;   
+    int tamano;   
 } ListaDoble;
 
 void Menu(){//Imprimir menu
@@ -23,6 +23,10 @@ void Menu(){//Imprimir menu
 
 void leerArchivo(char *nombre){ //Para leer el archivo e identificar catalogo y listas.
     FILE *ptrA = fopen(nombre, "r");
+    if (nombre == NULL) { //Verificación de que se pudo abrir el archivo
+        perror("Error al abrir");
+        return;
+    }
     
     if (ptrA == NULL){
         printf("Error: No se pudo abrir el archivo '%s'\n", nombre);
